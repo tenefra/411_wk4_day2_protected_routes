f9f9cc06568dd8e7cfc427e387377bee78495758
tenefra <stenefra@gmail.com>
2021-08-18 22:27:43 -0500

    completed commit

--- src/Router.js
+++ src/Router.js
@@ -1,27 +1,33 @@
-import React from 'react'
-import { Switch, Route, Redirect } from 'react-router'
-import cookie from 'cookie'
-import Home from './components/Home'
-import About from './components/About'
-import Car from './components/Car'
-import Login from './components/Login'
+import React from "react"
+import { Switch, Route, Redirect } from "react-router"
+import cookie from "cookie"
+import Home from "./components/Home"
+import About from "./components/About"
+import Car from "./components/Car"
+import Login from "./components/Login"
 
 // Write checkAuth function here
 // Check the cookies for a cookie called "loggedIn"
-
+const checkAuth = () => {
+  const cookies = cookie.parse(document.cookie)
+  return cookies["loggedIn"] ? true : false
+}
 
 // Write ProtectedRoute function here
 
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  return <Route {...rest} render={props => (checkAuth() ? <Component {...props} /> : <Redirect to="/login" />)} />
+}
 
 const Router = () => {
-    return (
-        <Switch>
-            <Route path="/login" component={Login} />
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/car/:id" component={Car} />
-        </Switch>
-    );
-};
+  return (
+    <Switch>
+      <Route path="/login" component={Login} />
+      <ProtectedRoute path="/about" component={About} />
+      <ProtectedRoute path="/" component={Home} />
+      <ProtectedRoute path="/car/:id" component={Car} />
+    </Switch>
+  )
+}
 
-export default Router;
+export default Router
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,25 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router'
-import {
-  TextField,
-  Button,
-  Container
-} from '@material-ui/core'
+import React, { Component } from "react"
+import { Redirect } from "react-router"
+import cookie from "cookie"
+import { TextField, Button, Container } from "@material-ui/core"
 
 class App extends Component {
   state = {
-    username: '',
-    password: ''
+    username: "",
+    password: ""
   }
 
-  handleTextChange = (e) => {
+  handleTextChange = e => {
     const state = { ...this.state }
     state[e.target.name] = e.target.value
     this.setState(state)
   }
 
-  login = (e) => {
+  login = e => {
     e.preventDefault()
     // set cookie here
     // set loggedIn = true and max-age = 60*1000 (one minute)
-
+    document.cookie = "loggedIn=true;max-age=60*1000"
     window.location.replace("/")
   }
 
@@ -31,30 +28,16 @@ class App extends Component {
       <div className="App">
         <Container maxWidth="sm">
           <form className="login-form" onSubmit={this.login}>
-            <TextField
-              required
-              onChange={this.handleTextChange}
-              value={this.state.username}
-              name="username"
-              label="Username"
-              type="text" />
-            <TextField
-              required
-              onChange={this.handleTextChange}
-              value={this.state.password}
-              name="password"
-              label="Password"
-              type="password" />
-            <Button
-              type="submit"
-              className="login-button"
-              variant="contained"
-              color="primary">Login</Button>
+            <TextField required onChange={this.handleTextChange} value={this.state.username} name="username" label="Username" type="text" />
+            <TextField required onChange={this.handleTextChange} value={this.state.password} name="password" label="Password" type="password" />
+            <Button type="submit" className="login-button" variant="contained" color="primary">
+              Login
+            </Button>
           </form>
         </Container>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
